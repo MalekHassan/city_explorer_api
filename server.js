1935b54945e63f955c12d908ca4025951712e995
MalekHassan <malekdalalha1995@gmail.com>
2020-08-30 15:46:44 +0300

    doing the weather thing code

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 'use strict'
 const express = require('express');
+const { request, response } = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3030;
 const app = express();
-
+app.use(cors());
 app.get('/location',(request,response) =>{
     const loca = require(`./data/location.json`);
     const cityName=request.query.city;
+    console.log('Hi query',request.query)
     let locationData = new Location (cityName,loca);
     response.send(locationData);
 })
+
+app.get('/weather',(request,response)=>{
+    const dataFile=require(`./data/weather.json`);
+    let dataArray=[];
+    // const weteherQuery=request.query.city;
+    dataFile.data.forEach(item => {
+    let weatherData= new Weather(item);
+    console.log('item',item)
+    dataArray.push(weatherData);
+    console.log("hello",weatherData)
+    })
+    response.send(dataArray);   
+})
+
+function Weather(item){
+    
+       this.forecast=item.weather.description;
+       this.time=item.datetime;
+    
+}
 
 function Location(cityName,loca){
     this.search_query=cityName;
