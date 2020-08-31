76457ea5e75c7880083ebb684184fa5e1073cc41
MalekHassan <malekdalalha1995@gmail.com>
2020-08-31 20:03:54 +0300

    doing the weather root

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 const cors = require('cors');
 const superagent = require('superagent');
 
-const PORT = process.env.PORT || 3030;
+const PORT =  3030;
 const app = express();
 app.use(cors());
 
 app.get('/location',locationSet);
-app.all('*',notFound);
-app.all('*',errorHandler);
+// app.all('*',notFound);
+// app.all('*',errorHandler);
 function locationSet(request,response){
         const cityName=request.query.city;
         // console.log('Hi query',request.query)
@@ -37,28 +37,56 @@ function locationSet(request,response){
 //     response.send(locationData);
 // })
 
-app.get('/weather',(request,response)=>{
-    const dataFile=require(`./data/weather.json`);
-    let dataArray=[];
-    // const weteherQuery=request.query.city;
-    dataFile.data.forEach(item => {
-    let weatherData= new Weather(item);
-    // console.log('item',item)
-    dataArray.push(weatherData);
-    // console.log("hello",weatherData)
+app.get('/weather', weatherFunc);
+function weatherFunc ( req , res) {
+    let weatherArr = []
+    const cityData = req.query.search_query;
+    const latit = req.query.latitude;
+    const long = req.query.longitude;
+    // console.log(cityData)
+    // console.log('latitude',latit)
+    // const weatherInfo = require('./data/weather.json');
+    // console.log(weatherInfo);
+    let weatherKey = process.env.WEATHER_KEY;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityData}&lat=${latit}&lon=${long}&key=${weatherKey}`
+    console.log("weatherkey" , url);
+     superagent.get(url)
+    .then(data =>{
+        //  console.log(data.body);
+        // let weatherArr = [];
+        // console.log('this is data',data)
+        let dataMap = data.body.data.map((item,idx)=>{
+        // let weatherArr = [];
+            // if (idx <= 7) {
+               const weatherat = new Weather (item)
+               weatherArr.push(weatherat);
+            // }
+            // console.log(weatherat);
+            // return weatherat
+        });
+                    // console.log(weatherArr);
+                    res.send(weatherArr);
+
     })
-    response.send(dataArray);   
-})
+    
+
+// let numbers = [2, 3, 4, 5];
+// let weatherMap = weatherInfo.data.map((item,idx)=>{
+//   let forecast = element.weather.description;
+    // let time = element.datetime;
+    // weatherArr.push(new Weather (forecast,time))
+// });
+};
 
 // 404 error
-function notFound (error,request, response) {
-    response.status(404).send(error);
-};
+// function notFound (error,request, response) {
+//     response.status(404).send(error);
+// };
 
 // 500 error
-function errorHandler (error,request, response) {
-    response.status(500).send(erorr);
-};
+// function errorHandler (error,request, response) {
+//     response.status(500).send(error);
+// };
 
 function Weather(item){
     
