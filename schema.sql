4e246fb8b9226ad363cc6f6a9ff5039d2b518b10
MalekHassan <malekdalalha1995@gmail.com>
2020-09-02 00:00:08 +0300

    initail submit

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS location;
+
+CREATE TABLE location (
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255),
+    latitude VARCHAR(255),
+    longitude VARCHAR(255)
+);
+
+-- INSERT INTO location VALUES ('Irbid','irbid-jordan','21','66');
+-- INSERT INTO location VALUES ('amman','amman-jordan','21','66');
+-- INSERT INTO location VALUES ('seattle','seattle-jordan','21','66');
